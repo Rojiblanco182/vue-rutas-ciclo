bbfdf3e7568c59dee800e99aee98313cfbb6ffba
Roberto Salinas <roberto.salinas@innocv.com>
2022-01-11 10:49:18 +0100

    added navBar and router-link to navigate

--- src/modules/pokemon/pages/PokemonPage.vue
+++ src/modules/pokemon/pages/PokemonPage.vue
@@ -1,3 +1,42 @@
 <template>
-  <h1>Pokemon page</h1>
+  <h1>Pokemon # {{ id }}</h1>
+  <div v-if="pokemon">
+    <img :src="pokemon.sprites.front_default" :alt="pokemon.name" />
+  </div>
 </template>
+
+<script>
+export default {
+  props: {
+    id: {
+      type: Number,
+      required: true
+    }
+  },
+  data() {
+    return {
+      pokemon: null
+    }
+  },
+  created() {
+    this.getPokemon()
+  },
+  methods: {
+    async getPokemon() {
+      try {
+        const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${this.id}`).then(r => r.json())
+        this.pokemon = pokemon
+        console.log(pokemon)
+      } catch (error) {
+        this.$router.push('/')
+        console.log('Pokemon not found')
+      }
+    }
+  },
+  watch: {
+    id() {
+      this.getPokemon()
+    }
+  }
+}
+</script>
---
+++ src/modules/shared/components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+      <router-link :to="{ name: 'home' }">Pokemon List</router-link>
+      <router-link :to="{ name: 'about' }">About</router-link>
+      <router-link :to="{ name: 'pokemon-id', params: { id: 4 } }">Pokemon by ID</router-link>
+  </div>
+</template>
+
+<style scoped>
+    div {
+        padding: 0 30px;
+    }
+
+    div a {
+        font-weight: bold;
+        margin: 0 10px;
+        color:darkolivegreen;
+    }
+
+    div .router-link-exact-active {
+        color:rgb(46, 199, 41)
+    }
+</style>
